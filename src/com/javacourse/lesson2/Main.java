fe900cfdb1be6dac49427d4704e6b40e35e7bbf6
Viktoriy Khalankova <vik.khalankova@mail.ru>
2022-06-27 20:49:20 +0300

    home

--- src/com/javacourse/lesson2/Main.java
+++ src/com/javacourse/lesson2/Main.java
@@ -2,7 +2,7 @@ package com.javacourse.lesson2;
 
 public class Main {
     public static void main(String[] args) {
-        int p =50;
+        int p =5;
         int u=16;
             if (p>u)
             System.out.println (p + "больше");
